c0e6ada6a1b6c334638b471dedba985aeb0b2a7c
Sebastian Hoitz <hoitz@komola.de>
2011-10-24 15:11:27 +0200

    Moved todo resource to his own controller. Added more logic

--- src/controller/index_controller.erl
+++ src/controller/index_controller.erl
@@ -1,11 +1,6 @@
 -module(todomanager_index_controller, [Req]).
 -compile(export_all).
 
-
 index('GET', []) ->
 	Todos = boss_db:find(todo, []),
-	{ok, [{todos, Todos}]};
-
-index('GET', ["format", Format]) ->
-	Todos = boss_db:find(todo, []),
-	{json, [{todos, Todos}]}.
+	{ok, [{todos, Todos}]}.
---
+++ src/controller/todo_controller.erl
@@ -0,0 +1,30 @@
+-module(todomanager_todo_controller, [Req]).
+-compile(export_all).
+
+%% GET todo/index
+index('GET', []) ->
+	Todos = boss_db:find(todo, []),
+	{json, [{todos, Todos}]};
+
+%% GET todo/read/123
+read('GET', [Id]) ->
+	Todo = boss_db:find(Id),
+	% TODO for some reason, when we have a non-existent todo, we still output the json
+	% data and don't jump to the not_found section.
+	case Todo of
+		Todo ->
+			{json, [{todo, Todo}]};
+		[] ->
+			not_found
+	end.
+
+%% POST /todo/create
+create('POST', []) ->
+	Todo = todo:new(id, Req:post_param("subject")),
+	{json, [{todo, element(2, Todo:save())}]}.
+
+%% POST /todo/update
+update('POST', [Id]) ->
+	Todo = boss_db:find(Id),
+	NewTodo = Todo:subject(Req:post_param("subject")),
+	{json, [{todo, element(2, NewTodo:save())}]}.
